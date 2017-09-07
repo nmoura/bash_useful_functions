18dfcc8a27216c8289a88ef8b5b6e95cb526acfc
Nilton Moura <moura.nilton@gmail.com>
2017-09-06 23:46:29 -0300

    corrected comment

--- tests_for_useful_functions/parser_section_items_test.sh
+++ tests_for_useful_functions/parser_section_items_test.sh
@@ -2,7 +2,7 @@
 source ../useful_functions
 conf_file=./parser_section_items_test.conf
 
-# Get the section1 items in the configuration file
+# Get the section2 items in the configuration file
 parser_section_items section2
 
 # Uncomment the line below to test from the command line
